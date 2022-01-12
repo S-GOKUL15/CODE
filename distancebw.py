420657597c7a5fcb77b80d45a5f42e3bddaaa291
GOKUL S <97543836+S-GOKUL15@users.noreply.github.com>
2022-01-12 12:00:36 +0530

    PYTHON PROGRAM TO FIND DISTANCE BETWEEN TWO POINTS

---
+++ distancebw.py
@@ -0,0 +1,6 @@
+x1=int(input("Enter the Value: "))
+x2=int(input("Enter the Value: "))
+y1=int(input("Enter the Value: "))
+y2=int(input("Enter the Value: "))
+distance=((x2-x1)**2 + (y2-y1)**2)**0.5
+print(round(distance,4))
