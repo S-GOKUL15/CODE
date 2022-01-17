63580b11f9b67611dfc77ae91cbb429562b58da0
GOKUL S <97543836+S-GOKUL15@users.noreply.github.com>
2022-01-17 20:04:42 +0530

    PYTHON PROGRAM TO CONVERT CELSIUS INTO FAHRENHEIT.

---
+++ celsiustofahren.py
@@ -0,0 +1,4 @@
+#python program to convert temperature in celsius and convert it to Fahrenheit
+celsius=int(input("Enter the temperature in Celsius:"))
+fahrenheit=(celsius*1.8)+32
+print("Temperature in Fahrenheit is:",fahrenheit)
